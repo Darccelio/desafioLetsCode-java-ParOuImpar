a81c098e2e75398deab07d266d6d01a565cd94ea
Darccelio Wildson <darccelio@gmail.com>
2021-12-13 00:15:39 -0300

    removendo linhas comentadas sem utilização

--- src/com/letscode/VetoresParOuImpar/service/Operacao.java
+++ src/com/letscode/VetoresParOuImpar/service/Operacao.java
@@ -7,20 +7,13 @@ public class Operacao {
 
     private static int tam = 5;
     private static int[] array = new int[tam];
-    private static int[] novoArray = new int[tam];
 
     public static void armazenarArray() {
         for( int i=0; i<tam; i++ ) {
             array[i] = Leitor.escanearEntradaNumeros();
         }
     }
-//    public static void arrayNumPares() {
-//        for( int i=0; i<tam; i++ ) {
-//            if(array[i] % 2 == 0) {
-//                novoArray[] =
-//            }//
-//        }//
-//    }
+
     public static void imprimirImpares() {
         Impressora.setMostrarNumImpares();
         for( int i=0; i<tam; i++ ) {
